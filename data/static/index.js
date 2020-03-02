f6c7afbaf6d5924bfedc5f511f228be53175b253
Emrah Oner <emrahoner@gmail.com>
2020-03-02 19:38:04 +1100

    Create static data source

---
+++ data/static/index.js
@@ -0,0 +1,14 @@
+const books = [
+    {
+        title: 'Kuvayi Milliye',
+        author: 'Nazim Hikmet'
+    },
+    {
+        title: 'Ince Memed',
+        author: 'Yasar Kemal'
+    }
+]
+
+export {
+    books
+}
