2299cddeb98c874352f906fe2ee3841d1c4db7df
Emrah Oner <emrahoner@gmail.com>
2020-03-10 19:51:46 +1100

    Information about Datasources

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ const resolvers = require('./resolvers')
 const typeDefs = require('./schema')
 const schemaDirectives = require('./directives')
 
+// For rest data source, RESTDataSource class can be extended from 'apollo-datasource-rest' package.
+// The class has methods for each http methods and rest clients can be created with extending it.
+// In willSendRequest function, common logics for every request can be implemented (like setting authorization header)
+// In resolveURL function, logic can be implemented to set the url for the client. (like fetching url from configuration)
+
+// There are also datasource packages for SQL and MongoDB 
+
 const server = new ApolloServer({ 
     typeDefs, 
     resolvers,
     schemaDirectives,
+    dataSources: () => {
+        return {
+            someAPI,
+            dbClient
+        }
+    },
     context: ({ req }) => ({
         token: req.headers.authorization
     }),
